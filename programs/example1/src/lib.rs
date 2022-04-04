d59dc61e7c3a58cc5c042e77e5968ee05d200b45
lawmeskiviahs <shaiviksemwal@gmail.com>
2022-04-04 20:26:09 +0530

    structures defined

--- programs/example1/src/lib.rs
+++ programs/example1/src/lib.rs
@@ -1,40 +1,134 @@
 use anchor_lang::prelude::*;
+// use anchor_spl::token::{self, Mint, TokenAccount, Transfer, MintTo};
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    // entrypoint,
+    // entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+    program::invoke,
+    system_instruction,
+};
 
-declare_id!("7CiTJHXzv7SdK1bf14nmCnPg9c8HRs3H9DdfEJMoay5K");
+declare_id!("4Y8PGKfY7q5hxDA17h6UHk5eACo6k9idy1chiHb7HKsp");
 
 #[program]
 mod example1 {
     use super::*;
-
-    pub fn create(ctx: Context<Create>) -> ProgramResult {
-        let base_account = &mut ctx.accounts.base_account;
-        base_account.count = 0;
+    pub fn init(ctx :Context <Init>) -> ProgramResult {
+        let plan = &mut ctx.accounts.plans;
+        let mut x:[Plan;6];
+        // let owner:Pubkey=;
+        let seed = "LazyInvestor2";
+        // let pubkey= Pubkey::;
+        // let vault = Pubkey::create_with_seed(base, seed, owner)?;
         Ok(())
     }
-
-    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
-        let base_account = &mut ctx.accounts.base_account;
-        base_account.count += 1;
+    pub fn deposit(ctx: Context<DepositCTX>, amount: u64) -> ProgramResult {
+        
+        let user = &ctx.accounts.pool;
+        let pool = &mut ctx.accounts.vault;
+        anchor_lang::solana_program::program::invoke(
+            &system_instruction::transfer(
+                user.to_account_info().key,
+                pool.to_account_info().key,
+                amount,
+            ),
+            &[
+                user.to_account_info(),
+                pool.to_account_info(),
+            ],
+        )?;
         Ok(())
     }
+    // pub fn withdraw() -> ProgramResult {
+    //     Ok(())
+    // }
 }
 
 #[derive(Accounts)]
-pub struct Create<'info> {
-    #[account(init, payer = user, space = 16 + 16)]
-    pub base_account: Account<'info, BaseAccount>,
+pub struct Init<'info> {
+    #[account(init, payer=user, space=264)]
+    pub plans: Account<'info, Plan>,
+    #[account(init, payer=user, space=264)]
+    pub wallet_account_mapping: Account<'info, Mapping>,
     #[account(mut)]
-    pub user: Signer<'info>,
-    pub system_program: Program<'info, System>,
+    pub user:Signer<'info>,
+    pub system_program: Program<'info, System>
+}
+#[account]
+pub struct Plan {
+    time : u64,
+    percent : u8
+}
+#[account]
+pub struct Mapping {
+    userWalletPubKey : Pubkey,
+    userAccountPubKey : Pubkey
 }
 
 #[derive(Accounts)]
-pub struct Increment<'info> {
+pub struct DepositCTX<'info> {
     #[account(mut)]
-    pub base_account: Account<'info, BaseAccount>,
+    depositor: Account<'info, MyAccount>,
+    // #[account(mut)]
+    // refferrer: Account<'info, MyAccount>,
+}
+#[account]
+pub struct MyAccount {
+    deposits: Vec<Deposit>,
+    checkpoint: u128,
+    referrer: Pubkey,
+    levels:[u8;3],
+    bonus: u32,
+    totalbonus:u32
 }
 
 #[account]
-pub struct BaseAccount {
+pub struct Deposit {
+        plan:u8,
+        percent:u128,
+        amount:u128,
+        profit:u128,
+        start:u128,
+        finish:u128
+}
+
+#[account]
+pub struct DepositAccount {
     pub count: u64,
+}
+
+#[account]
+pub struct Vault {
+    pub bump: u8,
+    pub payer: Pubkey,
+    pub mint_token: Pubkey, 
+    pub vault_token: Pubkey,
+    pub vault_mint: Pubkey,
+}
+
+
+
+
+#[derive(Accounts)]
+#[instruction(pool_name: String, payment: u64)]
+pub struct PayPool<'info>{
+    #[account(mut)]
+        ///// CHECK: xyz
+
+    pub pool: Account<'info, Pool>,
+    /// CHECK: xyz
+    #[account(mut)]
+    pub vault: Signer<'info>,
+}
+
+#[account]
+pub struct Pool {
+    pub pool_owner: Pubkey,
+    pub bump: u8,
+    pub capacity: u16,
+    pub name: String,
+    pub payers: Vec<Pubkey>,
 }
