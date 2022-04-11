65097ed2c902f757d80e3a8306d9090ab10aeb5a
lawmeskiviahs <shaiviksemwal@gmail.com>
2022-04-11 21:54:17 +0530

    invest function modified

--- programs/example1/src/lib.rs
+++ programs/example1/src/lib.rs
@@ -2,7 +2,8 @@ use anchor_lang::prelude::*;
 use solana_program::entrypoint::ProgramResult;
 use solana_safe_math::{SafeMath};
 use solana_program::{
-    account_info::{next_account_info, AccountInfo},
+    account_info::{AccountInfo},
+    // next_account_info,
     // entrypoint,
     // entrypoint::ProgramResult,
     msg,
@@ -27,12 +28,11 @@ mod example1 {
     pub fn init(ctx :Context <Init>) -> ProgramResult {
         msg!("Initialize");
         // let plan = &mut ctx.accounts.plans;
-        let pool = &mut ctx.accounts.vault;
+        // let pool = &mut ctx.accounts.vault;
         Ok(())
     }
-    pub fn deposit(ctx: Context<DepositCTX>, amount: u64) -> ProgramResult {
-        
-        let user = &ctx.accounts.depositor;
+    pub fn deposit(ctx: Context<DepositCTX>, amount: u64, referrer: Pubkey) -> ProgramResult {
+        let user = &mut ctx.accounts.depositor;
         let pool = &mut ctx.accounts.vault;
         anchor_lang::solana_program::program::invoke(
             &system_instruction::transfer(
@@ -47,36 +47,95 @@ mod example1 {
             )?;
             let mut fee = amount.safe_mul(PROJECT_FEE)?;
             fee = fee.safe_div(PERCENTS_DIVIDER)?;
+
             //(transfer sol to commissioner wallet)
             
-            // if (user.referrer) {
-                // 	if (users[referrer].deposits.length > 0 && referrer != msg.sender) {
-                    // 		user.referrer = referrer;
-                    // }
-                    
-                    Ok(())
-            }
-                // pub fn withdraw(ctx: Context<WithdrawCTX>) -> ProgramResult {
-                //     let mut plans:[Plan;6];
-                //         Ok(())
+            // if (user.referrer == null) {
+            // 	if (users[referrer].deposits.length > 0 && referrer != msg.sender) {
+            		user.referrer = referrer;
+            // }
+
+            let upline=user.referrer;
+            // for i in 1 .. 3 {
+			// 	if (upline != address(0)) {
+			// 		users[upline].levels[i] = users[upline].levels[i].safe_add(1);
+			// 		upline = users[upline].referrer;
+			// 	} else break;
+			// }
+
+            // if (user.referrer != address(0)) {
+                // address upline = user.referrer;
+                // for i in 1 .. 3 {
+                    // if (upline != address(0)) {
+                    //     let mut refamount = amount.safe_mul(REFERRAL_PERCENTS[i]);
+                    //      refamount = refamount.sef_div(PERCENTS_DIVIDER);
+                    //     users[upline].bonus = users[upline].bonus.safe_add(refamount);
+                    //     users[upline].totalBonus = users[upline].totalBonus.add(refamount);
+                    //     // emit RefBonus(upline, amount, i, refamount);
+                    //     upline = users[upline].referrer;
+                    // } else {break};
                 // }
+    
+            // }
+            // if (user.deposits.length() == 0) {
+            //     user.checkpoint = block.timestamp;
+            //     // emit Newbie(msg.sender);
+            // }
+            let plan = 2;
+            let x = getResult(plan, amount);
+                    Ok(())
     }
-                
-    #[derive(Accounts)]
-    pub struct Init<'info> {
-    #[account(init, payer=user, space=264)]
-    pub users: Account<'info, Mapping>,
-    #[account(init, payer=user, space=264)]
-    pub vault: Account<'info, Vault>,
-    #[account(mut)]
-    pub user:Signer<'info>,
-    pub system_program: Program<'info, System>
+    
+   
 }
+            
+pub fn getResult(plan:u8, amount:u64) -> (u32, u32, u32) {
+    let percent = 0; //getPercent(plan); safe_mul(percent); 
+    
+    let x: getdata = getdata{
+        percent: 2,
+        profit:4,
+        finish:6
+    };
+
+    // if (plan < 3) {
+        // let mut profit = deposit.safe_mul(percent);
+        // profit = profit.safe_div(PERCENTS_DIVIDER);
+        // profit=profit.safe_mul(plans[plan].time);
+        // } else if (plan < 6) {
+            // for i in 1 .. plans[plan].time {
+                //  let mut adder = (deposit.safe_add(profit));
+                // adder = adder.safe_mul(percent);
+                // adder = adder.safe_div(PERCENTS_DIVIDER);
+                //         // profit = profit.safe_add(adder);
+                //     // }
+                // }
+                
+                // let finish = block.timestamp.add(plans[plan].time.mul(TIME_STEP));
+                return (x.percent, x.profit, x.finish);
+}     
+pub struct getdata {
+    percent : u32,
+    profit: u32,
+    finish: u32 
+     }
+#[derive(Accounts)]
+pub struct Init<'info> {
+#[account(init, payer=user, space=264)]
+pub users: Account<'info, Mapping>,
+#[account(init, payer=user, space=264)]
+pub vault: Account<'info, Vault>,
+#[account(mut)]
+pub user:Signer<'info>,
+pub system_program: Program<'info, System>
+}
+
 #[account]
 pub struct Plan {
     time : u64,
     percent : u16
 }
+
 #[derive()]
 #[account]
 pub struct Mapping {
@@ -124,9 +183,6 @@ pub struct Vault {
     pub vault_token: Pubkey,
     pub vault_mint: Pubkey,
 }
-
-
-
 
 #[derive(Accounts)]
 #[instruction(pool_name: String, payment: u64)]
